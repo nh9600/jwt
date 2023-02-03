e3b496117c10387a85bddb794d4fb51c7ce3b940
nh9600 <nh9600@daum.net>
2023-02-03 23:21:18 +0900

    초기 셋팅

---
+++ src/main/java/com/sky/jwt/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.sky.jwt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig  {
+
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/h2-console/**", "/favicon.ico");
+    }
+
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/api/hello").permitAll()
+                .anyRequest().authenticated();
+        return http.build();
+    }
+}
